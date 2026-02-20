dfbefd12d79f265789d0b530ab66797b541cd55c
Lyle He <imilesho@gmail.com>
2026-02-19 23:47:10 -0500

    feat: add solution for Best Time to Buy and Sell Stock problem

---
+++ src/01_07_0121_Best_Time_to_Buy_and_Sell_Stock.py
@@ -0,0 +1,22 @@
+"""
+# Best Time to Buy and Sell Stock
+
+- **ID:** 121
+- **Difficulty:** EASY
+- **Topic Tags:** Array, Dynamic Programming
+- **Link:** [LeetCode Problem](https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/)
+"""
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        minLeft = prices[0]
+        res = 0
+        for x in prices:
+            if minLeft > x:
+                minLeft = x
+            res = max(res, x - minLeft)
+        return res
+
+"""
+because we know the future in the price, this question is intented to calculate the ideal biggest profit
+"""
