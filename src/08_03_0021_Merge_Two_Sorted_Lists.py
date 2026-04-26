5f002e040a99dec0833ae22f6e4601e180ef98a9
Lyle He <imilesho@gmail.com>
2026-04-26 14:53:17 -0400

    feat: implement mergeTwoLists function to merge two sorted linked lists

--- src/08_03_0021_Merge_Two_Sorted_Lists.py
+++ src/08_03_0021_Merge_Two_Sorted_Lists.py
@@ -13,6 +13,38 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    # def printList(self, list: Optional[ListNode]):
+    #     while list:
+    #         print(list.val, end=", ")
+    #         list = list.next
+    #     print()
+
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        pass
+        if not list1:
+            return list2
+        if not list2:
+            return list1
+        # self.printList(list1)
+        # self.printList(list2)
+        head = ListNode()
+        p = head
+        while list1 and list2:
+            # self.printList(head)
+            if list1.val <= list2.val:
+                p.next = list1
+                list1 = list1.next
+                p = p.next
+                p.next = None
+            else:
+                p.next = list2
+                list2 = list2.next
+                p = p.next
+                p.next = None
+        if list1:
+            p.next = list1
+        elif list2:
+            p.next = list2
+        return head.next
+
+
 
